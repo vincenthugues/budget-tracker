0683a38fcd8c072965dd50a4090f5c3a60d6b4cd
Vincent Hugues <vincenthugues.dev@gmail.com>
2022-09-11 20:07:05 +0200

    fix: add missing module dependencies

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Category, CategorySchema } from './schemas/category.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Category.name, schema: CategorySchema },
+    ]),
+  ],
   controllers: [CategoriesController],
-  providers: [CategoriesService]
+  providers: [CategoriesService],
 })
 export class CategoriesModule {}
