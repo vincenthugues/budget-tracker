cb84cea399db56fdc25054ec7c5d8259bd87d435
Vincent Hugues <vincenthugues.dev@gmail.com>
2022-10-26 23:32:28 +0200

    feat(client): enable account creation

--- client/src/components/Accounts.tsx
+++ client/src/components/Accounts.tsx
@@ -5,10 +5,59 @@ type Account = {
   name: string;
 };
 
+type AccountCreatorProps = {
+  onSubmit: Function;
+  onCancel: Function;
+};
+const AccountCreator = ({
+  onSubmit,
+  onCancel,
+}: AccountCreatorProps): JSX.Element => {
+  const [name, setName] = useState('');
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        onSubmit({ name });
+      }}
+    >
+      <label htmlFor="name">Name</label>
+      <input
+        id="name"
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        required
+      />
+      <button type="submit">Submit</button>
+      <button type="button" onClick={() => onCancel()}>
+        Cancel
+      </button>
+    </form>
+  );
+};
+
 const Accounts = (): JSX.Element => {
   const [items, setItems] = useState<Account[]>([]);
   const [isLoaded, setIsLoaded] = useState(false);
   const [error, setError] = useState<{ message: string } | null>(null);
+  const [showCreator, setShowCreator] = useState(false);
+
+  const onSubmit = async (accountToCreate: { name: string }) => {
+    const response = await fetch('/accounts', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(accountToCreate),
+    });
+    const createdAccount = await response.json();
+    setItems([...items, createdAccount]);
+
+    setShowCreator(false);
+  };
+  const onCancel = () => {
+    setShowCreator(false);
+  };
 
   useEffect(() => {
     fetch('/accounts')
@@ -26,16 +75,30 @@ const Accounts = (): JSX.Element => {
   }, []);
 
   if (error) {
-    return <div>Error: {error.message}</div>;
+    console.error(`Error: ${error.message}`);
+    return <div>Error when fetching data</div>;
   } else if (!isLoaded) {
     return <div>Loading...</div>;
   } else {
     return (
-      <ul>
-        {items.map(({ _id, name }) => (
-          <li key={_id}>{name}</li>
-        ))}
-      </ul>
+      <>
+        {showCreator ? (
+          <AccountCreator onSubmit={onSubmit} onCancel={onCancel} />
+        ) : (
+          <button
+            onClick={() => {
+              setShowCreator(true);
+            }}
+          >
+            ➕
+          </button>
+        )}
+        <ul>
+          {items.map(({ _id, name }) => (
+            <li key={_id}>{name}</li>
+          ))}
+        </ul>
+      </>
     );
   }
 };
