b1ae7659c277d5946b87b74bd1500c05a53285cd
Vincent Hugues <vincenthugues.dev@gmail.com>
2022-10-28 20:46:05 +0200

    feat(client): enable category creation and deletion

--- client/src/components/Categories.tsx
+++ client/src/components/Categories.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import Creator from './Creator';
 
 type Category = {
   _id: string;
@@ -9,6 +10,31 @@ const Categories = (): JSX.Element => {
   const [items, setItems] = useState<Category[]>([]);
   const [isLoaded, setIsLoaded] = useState(false);
   const [error, setError] = useState<{ message: string } | null>(null);
+  const [showCreator, setShowCreator] = useState(false);
+
+  const onSubmit = async (categoryToCreate: { name: string }) => {
+    const response = await fetch('/categories', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(categoryToCreate),
+    });
+    const createdCategory = await response.json();
+    setItems([...items, createdCategory]);
+
+    setShowCreator(false);
+  };
+  const onCancel = () => {
+    setShowCreator(false);
+  };
+  const onDelete = async (categoryId: string) => {
+    await fetch(`/categories/${categoryId}`, {
+      method: 'DELETE',
+    });
+    setItems(items.filter(({ _id }) => _id !== categoryId));
+  };
+  const categoryCreatorProperties = [
+    { name: 'name', label: 'Name', type: 'text' },
+  ];
 
   useEffect(() => {
     fetch('/categories')
@@ -26,16 +52,45 @@ const Categories = (): JSX.Element => {
   }, []);
 
   if (error) {
-    return <div>Error: {error.message}</div>;
+    console.log(`Error: ${error.message}`);
+    return <div>Error when fetching data</div>;
   } else if (!isLoaded) {
     return <div>Loading...</div>;
   } else {
     return (
-      <ul>
-        {items.map(({ _id, name }) => (
-          <li key={_id}>{name}</li>
-        ))}
-      </ul>
+      <>
+        {showCreator ? (
+          <Creator
+            onSubmit={onSubmit}
+            onCancel={onCancel}
+            properties={categoryCreatorProperties}
+          />
+        ) : (
+          <button
+            onClick={() => {
+              setShowCreator(true);
+            }}
+          >
+            ➕
+          </button>
+        )}
+        <ul>
+          {items.map(({ _id, name }) => (
+            <li key={_id}>
+              {name}{' '}
+              <button
+                onClick={() => {
+                  if (window.confirm(`Delete the category "${name}"?`)) {
+                    onDelete(_id);
+                  }
+                }}
+              >
+                ❌
+              </button>
+            </li>
+          ))}
+        </ul>
+      </>
     );
   }
 };
