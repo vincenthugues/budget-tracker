f85b726b7772a160f9dda7dd1ac8db6a9a853459
Vincent Hugues <vincenthugues.dev@gmail.com>
2022-11-18 23:46:38 +0100

    feat(server): increase body parser json limit

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { json } from 'body-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(json({ limit: '50mb' }));
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
