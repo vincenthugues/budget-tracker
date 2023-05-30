32e5fd3e6644bf8afc71893fb59119e43a5de5fb
Vincent Hugues <vincenthugues.dev@gmail.com>
2023-05-30 13:08:53 +0200

    feat(client): show transactions of last active month for MonthBudget

--- client/src/components/MonthBudget.tsx
+++ client/src/components/MonthBudget.tsx
@@ -11,9 +11,51 @@ type TransactionDraft = {
 
 type Transaction = TransactionDraft & { _id: string };
 
+const getLastTransactionYearMonth = (
+  transactions: Transaction[]
+): [year: number, month: number] | [] => {
+  if (!transactions?.length) return [];
+
+  const lastTransaction = transactions[transactions.length - 1];
+  const lastDate = new Date(lastTransaction.date);
+  return [lastDate.getFullYear(), lastDate.getMonth() + 1];
+};
+
+const sortByDate = (
+  arr: { date: Date | string }[]
+): { date: Date | string }[] => {
+  arr.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+  return arr;
+};
+
+const filterByYearAndMonth = (
+  transactions: Transaction[],
+  targetYear: number,
+  targetMonth: number
+) =>
+  transactions.filter(({ date }) => {
+    const [year, month] = new Date(date).toISOString().split('-');
+    return Number(year) === targetYear && Number(month) === targetMonth;
+  });
+
+const getMonthNameFromDate = (date: Date): string => {
+  return date.toLocaleString('default', { month: 'long' });
+};
+
 const MonthBudget = (): JSX.Element => {
   const [transactions, isLoading, errorMessage] =
     useFetchedResource<Transaction>('transactions');
+  sortByDate(transactions);
+
+  const [targetYear, targetMonth] = getLastTransactionYearMonth(transactions);
+  const lastMonthTransactions =
+    targetYear && targetMonth
+      ? filterByYearAndMonth(transactions, targetYear, targetMonth)
+      : [];
+  sortByDate(lastMonthTransactions);
+  const targetMonthName = lastMonthTransactions[0]
+    ? getMonthNameFromDate(new Date(lastMonthTransactions[0].date))
+    : null;
 
   if (errorMessage) {
     console.log(`Error: ${errorMessage}`);
@@ -23,6 +65,9 @@ const MonthBudget = (): JSX.Element => {
   } else {
     return (
       <>
+        <h2>
+          {targetMonthName} {targetYear}
+        </h2>
         <table>
           <tbody>
             <tr>
@@ -32,7 +77,7 @@ const MonthBudget = (): JSX.Element => {
               <th>Payee</th>
               <th>Amount</th>
             </tr>
-            {transactions.map(
+            {lastMonthTransactions.map(
               ({ _id, date, amount, accountId, payeeId, categoryId }) => (
                 <tr key={_id}>
                   <td>{getDisplayFormattedDate(date)}</td>
