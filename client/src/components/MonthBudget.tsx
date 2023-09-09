d538042e36cdffdbee645fa301c72592e763cffa
Vincent Hugues <vincenthugues.dev@gmail.com>
2023-09-09 23:53:53 +0200

    feat: display placeholder goals by month

--- client/src/components/MonthBudget.tsx
+++ client/src/components/MonthBudget.tsx
@@ -49,84 +49,127 @@ export const MonthBudget = ({
   accounts: Account[];
   payees: Payee[];
   categories: Category[];
-}): JSX.Element => (
-  <>
-    <h2>
-      {monthName} {year}
-    </h2>
-    <div>
-      Income: {getDisplayFormattedAmount(month.income)} - Budgeted:{' '}
-      {getDisplayFormattedAmount(month.budgeted || 0)} - To Be Budgeted:{' '}
-      {getDisplayFormattedAmount(month.toBeBudgeted || 0)} - Activity{' '}
-      {getDisplayFormattedAmount(month.activity || 0)}
-    </div>
-    <div>Total income: {getDisplayFormattedAmount(totalIncome)}</div>
-    <div>
-      Total spending: {getDisplayFormattedAmount(Math.abs(totalSpending))}
-    </div>
-    <div>
-      Net total: {getDisplayFormattedAmount(totalIncome + totalSpending)}
-    </div>
-    <div>
-      Goals
-      {month.goals.map(
-        ({
-          categoryId,
-          balance,
-          activity,
-          budgeted,
-          goalType,
-          startMonth,
-          endMonth,
-          target,
-        }) => (
-          <div>
-            {categories.find(({ _id }) => _id === categoryId)?.name} (
-            {GoalType[goalType]} - {getDisplayFormattedAmount(target)} between{' '}
-            {getMonthNameFromDate(startMonth)} and{' '}
-            {getMonthNameFromDate(endMonth)}): Balance{' '}
-            {getDisplayFormattedAmount(balance)} (
-            {getDisplayFormattedAmount(budgeted)} this month) - Activity{' '}
-            {getDisplayFormattedAmount(activity)} - Goal Type - Start Month{' '}
-          </div>
-        )
-      )}
-    </div>
-    <h3>Categories</h3>
-    <table>
-      <tbody>
-        <tr>
-          <th>Category</th>
-          <th>Total</th>
-        </tr>
-        {monthCategories.map(({ categoryName, total }) => (
-          <tr key={categoryName}>
-            <td>{categoryName}</td>
-            <td>{getDisplayFormattedAmount(total)}</td>
+}): JSX.Element => {
+  const getCategoryGoalDisplayInfo = (id: string) => {
+    const goal = month.goals.find(({ categoryId }) => categoryId === id);
+
+    if (!goal) return null;
+
+    return `${getDisplayFormattedAmount(
+      goal.balance
+    )}/${getDisplayFormattedAmount(goal.target)} (${getDisplayFormattedAmount(
+      goal.budgeted
+    )} this month)`;
+  };
+
+  return (
+    <>
+      <h2>
+        {monthName} {year}
+      </h2>
+      <div>
+        Income: {getDisplayFormattedAmount(month.income)} - Budgeted:{' '}
+        {getDisplayFormattedAmount(month.budgeted || 0)} - To Be Budgeted:{' '}
+        {getDisplayFormattedAmount(month.toBeBudgeted || 0)} - Activity{' '}
+        {getDisplayFormattedAmount(month.activity || 0)}
+      </div>
+      <div>Total income: {getDisplayFormattedAmount(totalIncome)}</div>
+      <div>
+        Total spending: {getDisplayFormattedAmount(Math.abs(totalSpending))}
+      </div>
+      <div>
+        Net total: {getDisplayFormattedAmount(totalIncome + totalSpending)}
+      </div>
+      <div>
+        Goals
+        {month.goals.map(
+          ({
+            categoryId,
+            balance,
+            activity,
+            budgeted,
+            goalType,
+            startMonth,
+            endMonth,
+            target,
+          }) => (
+            <div>
+              {categories.find(({ _id }) => _id === categoryId)?.name} (
+              {GoalType[goalType]} - {getDisplayFormattedAmount(target)} between{' '}
+              {getMonthNameFromDate(startMonth)} and{' '}
+              {getMonthNameFromDate(endMonth)}): Balance{' '}
+              {getDisplayFormattedAmount(balance)} (
+              {getDisplayFormattedAmount(budgeted)} this month) - Activity{' '}
+              {getDisplayFormattedAmount(activity)} - Goal Type - Start Month{' '}
+            </div>
+          )
+        )}
+      </div>
+      <h3>Categories</h3>
+      <table>
+        <tbody>
+          <tr>
+            <th>Category</th>
+            <th>Parent category</th>
+            <th>Goal</th>
+            <th>isHidden</th>
+            <th>isDeleted</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
-    <h3>Transactions</h3>
-    <table>
-      <tbody>
-        <tr>
-          <th>Date</th>
-          <th>Account</th>
-          <th>Payee</th>
-          <th>Category</th>
-          <th>Amount</th>
-        </tr>
-        {transactions.map((transaction) => (
-          <MonthTransactionRow
-            key={transaction._id}
-            transaction={transaction}
-            accounts={accounts}
-            payees={payees}
-            categories={categories}
-          />
-        ))}
-      </tbody>
-    </table>
-  </>
-);
+          {categories.map(
+            ({ _id, parentCategoryId, name, isHidden, isDeleted }) => (
+              <tr key={name}>
+                <td>{name}</td>
+                <td>
+                  {
+                    categories.find(
+                      (category) => category._id === parentCategoryId
+                    )?.name
+                  }
+                </td>
+                <td>{getCategoryGoalDisplayInfo(_id)}</td>
+                <td>{isHidden === true ? 'true' : 'false'}</td>
+                <td>{isDeleted === true ? 'true' : 'false'}</td>
+              </tr>
+            )
+          )}
+        </tbody>
+      </table>
+      <h3>Month categories</h3>
+      <table>
+        <tbody>
+          <tr>
+            <th>Category</th>
+            <th>Total</th>
+          </tr>
+          {monthCategories.map(({ categoryName, total }) => (
+            <tr key={categoryName}>
+              <td>{categoryName}</td>
+              <td>{getDisplayFormattedAmount(total)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <h3>Transactions</h3>
+      <table>
+        <tbody>
+          <tr>
+            <th>Date</th>
+            <th>Account</th>
+            <th>Payee</th>
+            <th>Category</th>
+            <th>Amount</th>
+          </tr>
+          {transactions.map((transaction) => (
+            <MonthTransactionRow
+              key={transaction._id}
+              transaction={transaction}
+              accounts={accounts}
+              payees={payees}
+              categories={categories}
+            />
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
